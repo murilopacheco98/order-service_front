f6c2f2332e12527a70121320c5baf76752a006b7
Murilo Pacheco <murilo.pacheco99@gmail.com>
2023-03-16 17:05:28 -0300

    refactor: nav opened/closed

--- src/app/views/components/template/nav/nav.component.ts
+++ src/app/views/components/template/nav/nav.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ViewChild } from '@angular/core';
+import { MatDrawer } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-nav',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
-
+  @ViewChild('sidenav') sidenav!: MatDrawer;
+  @HostListener('window:resize', ['$event'])
+  onResize(event: { target: { innerWidth: number; }; }) {
+      if (event.target.innerWidth < 1100) {
+          this.sidenav.close();
+      }
+      if (event.target.innerWidth > 1100) {
+         this.sidenav.open();
+      }
+  }
 }
